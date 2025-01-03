c8c38fbc2f44110a3a3d910cd62fd3286fe3c3de
Mario Calcagno <mario.calcagno@protonmail.com>
2025-01-03 11:03:03 +0100

    Aggiunti file per specifica dati e specifica informale

--- scripts/todo.pl
+++ scripts/todo.pl
@@ -4,6 +4,7 @@ use v5.38;
 
 my $dir = "."; # cartella corrente
 my @texfiles = glob($dir . "/*.tex"); # lista di file con estensione .tex
+my $i = 1;
 # per ogni file trovato:
 ##   apri il file
 ##   per ogni riga:
@@ -13,7 +14,9 @@ foreach (@texfiles) {
   open(my $in, "<", $_); # $in è il file handle creato da open
   while(<$in>){ # <$in> legge una riga dal file-handle $in
     if(/^\% TODO/) { # fa il match con stringhe che iniziano con "% TODO"
+      print $i . ": ";
       print $_; # semplice stampa
+      $i = $i +1;
     }
   }
   close($in); # chiude il file (evita leak)
